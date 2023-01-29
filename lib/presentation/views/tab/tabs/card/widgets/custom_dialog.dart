449eb8015cc4fca576f328ce376329dfcb808351
Asadbek <106092435+AsadbekMobileDeveloper@users.noreply.github.com>
2023-01-29 22:25:52 +0500

    DeleteDialog update

--- lib/presentation/views/tab/tabs/card/widgets/custom_dialog.dart
+++ lib/presentation/views/tab/tabs/card/widgets/custom_dialog.dart
@@ -1,22 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_contest/presentation/utils/constants/color_const.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-void showDeleteDialog(BuildContext context, {required VoidCallback onDeleteTap}) {
+void showDeleteDialog(BuildContext context,
+    {required VoidCallback onDeleteTap}) {
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return AlertDialog(
-        title: const Text("Haqiqatan o'chirmoqchimisiz?"),
-        actions: <Widget>[
-          ElevatedButton(
-            child: Text('Cancel'),
-            onPressed: () {
-              Navigator.of(context).pop();
-            },
-          ),
-          ElevatedButton(
-            child: Text('Delete'),
-            onPressed: onDeleteTap,
-          ),
+        backgroundColor: Colors.white,
+        title: const Text(
+          "Haqiqatan o'chirmoqchimisiz?",
+          textAlign: TextAlign.center,
+        ),
+        actions: [
+          SizedBox(
+            height: 30.h,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: ColorConst.primaryColor),
+                  child: Text(
+                    "Yo'q",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.sp,
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(12)),
+                      backgroundColor: ColorConst.primaryColor),
+                  onPressed: onDeleteTap,
+                  child: Text(
+                    "Ha",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20.sp,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       );
     },
