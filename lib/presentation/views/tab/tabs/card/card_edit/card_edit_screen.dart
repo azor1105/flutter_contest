e02dd99e812686bb5ac15cc74577ff1122cbd55b
azor1105 <azambekergashev1@gmail.com>
2023-01-29 21:46:09 +0500

    some changes on card

--- lib/presentation/views/tab/tabs/card/card_edit/card_edit_screen.dart
+++ lib/presentation/views/tab/tabs/card/card_edit/card_edit_screen.dart
@@ -36,108 +36,110 @@ class _CardEditScreenState extends State<CardEditScreen> {
   Widget build(BuildContext context) {
     var card = widget.card;
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text("Kartani tahrirlang!"),
       ),
-      body: Column(
-        children: [
-          CardItem(
-            cardType: card.cardType,
-            cardHolderName: card.owner,
-            expireDate: card.expireDate,
-            cardNumber: card.cardNumber,
-            cardName: card.cardName,
-            gradient: ColorConst.myGradients[selectedIndex],
-          ),
-          SizedBox(
-            height: 100.h,
-            child: ListView(
-              padding: EdgeInsets.symmetric(horizontal: 24.w),
-              shrinkWrap: true,
-              scrollDirection: Axis.horizontal,
-              children: List.generate(10, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    selectedIndex = index;
-                    setState(() {});
-                  },
-                  child: Stack(
-                    children: [
-                      Center(
-                        child: Container(
-                          height: 50.w,
-                          width: 50.w,
-                          margin: EdgeInsets.only(right: 8.w),
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            gradient: LinearGradient(
-                              colors: ColorConst.myGradients[index],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            CardItem(
+              cardType: card.cardType,
+              cardHolderName: card.owner,
+              expireDate: card.expireDate,
+              cardNumber: card.cardNumber,
+              cardName: card.cardName,
+              gradient: ColorConst.myGradients[selectedIndex],
+            ),
+            SizedBox(
+              height: 100.h,
+              child: ListView(
+                padding: EdgeInsets.symmetric(horizontal: 24.w),
+                shrinkWrap: true,
+                scrollDirection: Axis.horizontal,
+                children: List.generate(10, (index) {
+                  return GestureDetector(
+                    onTap: () {
+                      selectedIndex = index;
+                      setState(() {});
+                    },
+                    child: Stack(
+                      children: [
+                        Center(
+                          child: Container(
+                            height: 50.w,
+                            width: 50.w,
+                            margin: EdgeInsets.only(right: 8.w),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              gradient: LinearGradient(
+                                colors: ColorConst.myGradients[index],
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Visibility(
-                        visible: selectedIndex == index,
-                        child: const Positioned(
-                          bottom: 0,
-                          top: 0,
-                          left: 10,
-                          child: Icon(
-                            Icons.done,
-                            color: Colors.white,
+                        Visibility(
+                          visible: selectedIndex == index,
+                          child: const Positioned(
+                            bottom: 0,
+                            top: 0,
+                            left: 10,
+                            child: Icon(
+                              Icons.done,
+                              color: Colors.white,
+                            ),
                           ),
-                        ),
-                      )
-                    ],
-                  ),
-                );
-              }),
+                        )
+                      ],
+                    ),
+                  );
+                }),
+              ),
             ),
-          ),
-          MyTextField(
-            controller: cardNameC,
-            text: "Karta nomi",
-            onChanged: (String v) {
-              setState(() {});
-            },
-          ),
-          MyTextField(
-              controller: userNameC,
-              text: "Karta egasi to'liq ismi sharifi",
+            MyTextField(
+              controller: cardNameC,
+              text: "Karta nomi",
               onChanged: (String v) {
                 setState(() {});
-              }),
-          const Spacer(),
-          GlobalButton(
-            onTap: () async {
-              if (cardNameC.text.isNotEmpty && userNameC.text.isNotEmpty) {
-                var cardModel = CardModel(
-                  cardId: card.cardId,
-                  gradient: ColorConst.myGradients[selectedIndex],
-                  cardNumber: card.cardNumber,
-                  cardName: cardNameC.text,
-                  moneyAmount: card.moneyAmount,
-                  owner: userNameC.text,
-                  expireDate: card.expireDate,
-                  iconImage: card.iconImage,
-                  userId: card.userId,
-                  cardType: card.cardType,
-                  index: selectedIndex,
-                );
-                Utils.showProgress(context: context);
-                await CardRepo(fireStore: FirebaseFirestore.instance).updateCardData(cardModel: cardModel, docId: card.cardId);
-                Utils.getMyToast(message: "Muvaffaqiyatli yangilandi");
-                Navigator.pop(context);
-                Navigator.pop(context);
-              } else {
-                Utils.getMyToast(message: "Hammasini to'ldiring");
-              }
-            },
-            buttonText: "Yangilash",
-          ),
-        ],
+              },
+            ),
+            MyTextField(
+                controller: userNameC,
+                text: "Karta egasi to'liq ismi sharifi",
+                onChanged: (String v) {
+                  setState(() {});
+                }),
+            SizedBox(height: MediaQuery.of(context).size.height*0.2,),
+            GlobalButton(
+              onTap: () async {
+                if (cardNameC.text.isNotEmpty && userNameC.text.isNotEmpty) {
+                  var cardModel = CardModel(
+                    cardId: card.cardId,
+                    gradient: ColorConst.myGradients[selectedIndex],
+                    cardNumber: card.cardNumber,
+                    cardName: cardNameC.text,
+                    moneyAmount: card.moneyAmount,
+                    owner: userNameC.text,
+                    expireDate: card.expireDate,
+                    iconImage: card.iconImage,
+                    userId: card.userId,
+                    cardType: card.cardType,
+                    index: selectedIndex,
+                  );
+                  CardRepo(fireStore: FirebaseFirestore.instance).updateCardData(
+                    cardModel: cardModel,
+                    docId: card.cardId,
+                  );
+                  Utils.getMyToast(message: "Muvaffaqiyatli yangilandi");
+                  Navigator.pop(context);
+                } else {
+                  Utils.getMyToast(message: "Hammasini to'ldiring");
+                }
+              },
+              buttonText: "Yangilash",
+            ),
+          ],
+        ),
       ),
     );
   }
